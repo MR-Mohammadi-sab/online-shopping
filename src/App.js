b7857c9a6d5d4c337fc8f84acfaee1e9ca1408b6
MR-Mohammadi-sab <asf423796@gmail.com>
2024-07-26 05:54:22 -0700

    solve problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route ,} from 'react-router-dom'
 import Nav from './componete/nav';
 import { Shop } from './pages/shop/shop.jsx';
 import { Cart } from './pages/card/card.jsx';
@@ -15,7 +15,6 @@ function App() {
         <Welcome />
         <Routes>
         <Route path='/' element={<Shop />}/>
-          {/* <Route path='/online-shopping/' element={<Shop />}/> */}
           <Route path='/card' element={<Cart />}/>
         </Routes>
       </Router>
